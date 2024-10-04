e8423083b14980e9ea5637c2810ce696df4e88ec
Tomas <bekjon_98@mail.ru>
2024-10-04 22:36:05 +0300

    added tests

--- parser_test.go
+++ parser_test.go
@@ -33,3 +33,37 @@ func TestSimplArray(t *testing.T) {
 		t.Errorf("error parsing %v", err)
 	}
 }
+
+func TestNestedJson(t *testing.T) {
+
+	sample := []byte("{\"key\": { \"key1\":\"nested\" } }")
+
+	parser, err := NewParser(sample)
+
+	if err != nil {
+		t.Errorf("error instantiating parser %v", err)
+	}
+
+	t.Log("tokens", parser.tokens)
+	_, err = parser.Parse()
+	if err != nil {
+		t.Errorf("error parsing %v", err)
+	}
+}
+
+func TestNestedWithArrayJson(t *testing.T) {
+
+	sample := []byte("{\"key\": \"value\", \"key-n\": 101, \"key-o\": { \"inner key\": \"inner value\" }, \"key-l\": [\"list value\"] } ")
+
+	parser, err := NewParser(sample)
+
+	if err != nil {
+		t.Errorf("error instantiating parser %v", err)
+	}
+
+	t.Log("tokens", parser.tokens)
+	_, err = parser.Parse()
+	if err != nil {
+		t.Errorf("error parsing %v", err)
+	}
+}
